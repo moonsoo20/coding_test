4aaf7d530e4e54ad3a1468078f7cf316ce0fce27
moonsoo20 <98132695+moonsoo20@users.noreply.github.com>
2023-07-11 14:20:38 +0900

    7/11 P11659

---
+++ codingTest_ex/src/codingTest_ex/P11659_구간합구하기.java
@@ -0,0 +1,35 @@
+package codingTest_ex;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class P11659_구간합구하기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader= 
+				new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer stringTokenizer= new StringTokenizer(bufferedReader.readLine());
+		
+		int suNo= Integer.parseInt(stringTokenizer.nextToken());
+		int quizNo= Integer.parseInt(stringTokenizer.nextToken());
+		
+		long []S= new long[suNo+1]; //배열이 0부터가 아니라 1부터 받기 위해서
+		
+		 stringTokenizer= new StringTokenizer(bufferedReader.readLine());
+		
+		 for(int i=1; i<=suNo; i++) {
+			 S[i]=S[i-1]+ Integer.parseInt(stringTokenizer.nextToken());
+			 
+		 }
+		 for(int q=0; q<quizNo; q++) {
+			 stringTokenizer= new StringTokenizer(bufferedReader.readLine());
+			 
+			int i= Integer.parseInt(stringTokenizer.nextToken());
+			int j= Integer.parseInt(stringTokenizer.nextToken());
+			
+			System.out.println(S[j]-S[i-1]);
+		 }
+	}
+}
