e7a3f0e75cd3fed56356144807301480b9b486b7
moonsoo20 <98132695+moonsoo20@users.noreply.github.com>
2023-07-11 23:36:22 +0900

    7/11

---
+++ codingTest_ex/src/codingTest_ex/P15466_평균구하기.java
@@ -0,0 +1,26 @@
+package codingTest_ex;
+
+import java.util.Scanner;
+
+public class P15466_평균구하기 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		int N= sc.nextInt();
+		
+		long sum=0;
+		long max=0;
+		
+
+		for(int i=0; i<N; i++) {
+			int temp= sc.nextInt();
+			if(temp>max) max=temp;
+			sum= sum+temp;
+			
+		}
+		System.out.println(sum*100.0/max/N);
+		
+	}
+
+}
